b9a17b700c0a507d626e9d2e8905fae447d8d020
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2025-02-20 17:14:02 -0400

    Criado criação de token

---
+++ JwtAspNet/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace JwtAspNet
+{
+	public static class Configuration
+	{
+		public static string PrivateKey { get; set; } = "E1A96F77/9674-45D2-8B0C/(/*=)-73EBACCE01BA";
+    }
+}
--- JwtAspNet/Program.cs
+++ JwtAspNet/Program.cs
@@ -1,6 +1,26 @@
+using JwtAspNet.Models;
+using JwtAspNet.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<TokenService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (TokenService token) =>
+{
+	var user = new User(
+		1,
+		"Eder",
+		"eder.tes",
+		"https://",
+		"13546",
+		new[] { "admin", "estudante" }
+	);
+	token.Create(user);
+});
+
+//app.UseAuthentication();
+//app.UseAuthorization();
 
 app.Run();
