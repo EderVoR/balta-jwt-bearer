a22a700a98e721aa50a3e01be4ce9769c736a54f
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2025-02-21 16:50:26 -0400

    Criado entity e value objects

---
+++ JwtStore/JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -0,0 +1,13 @@
+﻿using JwtStore.Core.SharedContext.Extensions;
+using JwtStore.Core.SharedContext.ValueObjects;
+
+namespace JwtStore.Core.AccountContext.ValueObjects
+{
+	internal class Email : ValueObject
+	{
+        private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w)*\.\w+([-.]\w+)*$";
+
+        public string Adress { get; }
+        public string Hash => Adress.ToBase64();
+    }
+}
