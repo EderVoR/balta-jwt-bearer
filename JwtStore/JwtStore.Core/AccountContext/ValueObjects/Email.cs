428e6f69ad1cdd5b9a8a35890258d5db94afa323
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2025-02-25 16:23:59 -0400

    Value Object email

--- JwtStore/JwtStore.Core/AccountContext/ValueObjects/Email.cs
+++ JwtStore/JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -1,13 +1,40 @@
 ﻿using JwtStore.Core.SharedContext.Extensions;
 using JwtStore.Core.SharedContext.ValueObjects;
+using System.Text.RegularExpressions;
 
 namespace JwtStore.Core.AccountContext.ValueObjects
 {
-	internal class Email : ValueObject
+	public partial class Email : ValueObject
 	{
+        public Email(string adress)
+        {
+            if (string.IsNullOrEmpty(adress))
+                throw new Exception("Email inválido");
+
+            Adress = adress.Trim().ToLower();
+
+            if (Adress.Length < 5)
+                throw new Exception("Email inválido");
+
+            if (!EmailRegex().IsMatch(Adress)) 
+                throw new Exception("Email inválido");
+        }
+
         private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w)*\.\w+([-.]\w+)*$";
 
         public string Adress { get; }
         public string Hash => Adress.ToBase64();
+
+        public static implicit operator string(Email email)
+            => email.ToString();
+
+        public static implicit operator Email(string email)
+            => new Email(email);
+
+        public override string ToString()
+            => Adress;
+
+		[GeneratedRegex(Pattern)]
+        private static partial Regex EmailRegex();
     }
 }
