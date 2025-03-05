4a864751eef6d4eaf53d2c2f6e392d5790a7b6de
Eder Vieira <eder.edy@gmail.com>
2025-03-04 21:51:49 -0400

    Aplicando use cases

---
+++ JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Create/Specification.cs
@@ -0,0 +1,17 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create
+{
+    public static class Specification
+    {
+        public static Contract<Notification> Ensure(Request request)
+            => new Contract<Notification>()
+                .Requires()
+                .IsLowerThan(request.Name.Length, 160, "Name", "O nome deve ser minusculo")
+                .IsGreaterThan(request.Name.Length, 3, "Name", "O nome deve ser maior que 3 caracteres")
+                .IsLowerThan(request.Password.Length, 40, "Password", "A senha não deve ser superior a 40 caracteres")
+                .IsLowerThan(request.Password.Length, 8, "Password", "A senha deve conter no minimo 8 caracteres")
+                .IsEmail(request.Email, "Email", "Email inválido");
+    }
+}
---
+++ JwtStore/JwtStore.Core/Contexts/SharedContext/UseCases/Response.cs
@@ -0,0 +1,13 @@
+﻿using Flunt.Notifications;
+using System.Reflection;
+
+namespace JwtStore.Core.Contexts.SharedContext.UseCases
+{
+    public abstract class Response
+    {
+        public string Message { get; set; } = string.Empty;
+        public int Status { get; set; } = 400;
+        public bool IsSucess => Status >= 200 && Status <= 299;
+        public IEnumerable<Notification>? Notifications { get; set; }
+    }
+}
