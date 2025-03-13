18b5e17ce1fac36c54be938822473f22fe171d16
Eder Vieira de Oliveira Ribeiro <eder.edy@gmail.com>
2025-03-13 15:12:21 -0400

    Criado repositry e service

--- JwtStore/JwtStore.Core/Configuration.cs
+++ JwtStore/JwtStore.Core/Configuration.cs
@@ -3,13 +3,26 @@
 	public static class Configuration
 	{
 		public static SecretsConfiguration Secrets { get; set; } = new();
-		public static DatabaseConfiguration Database { get; set; } = new();
+		public static EmailConfiguration Email { get; set; } = new();
+		public static SendgridConfiguration SendGrid { get; set; } = new();
+        public static DatabaseConfiguration Database { get; set; } = new();
 
 
 		public class DatabaseConfiguration
 		{
 			public string ConnectionString { get; set; } = string.Empty;
 		}
+
+		public class EmailConfiguration
+		{
+			public string DefaultFromEmail { get; set; } = "eder_edy@hotmail.com";
+			public string DefaultFromName { get; set; } = "Eder Teste";
+        }
+
+		public class SendgridConfiguration
+		{
+			public string ApiKey { get; set; } = string.Empty;
+        }
 
 		public class SecretsConfiguration
 		{
